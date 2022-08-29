675d36446b28ebe6ffcc9b2aea7388379cbe9931
dhmk083 <dhmk083@gmail.com>
2022-08-29 17:15:15 +0300

    add `signal` improvement ideas

--- src/signal.ts
+++ src/signal.ts
@@ -13,3 +13,30 @@ export function signal<T = void>() {
 
   return self;
 }
+
+// todo: signal improvement (multi args + customizable)
+
+// export type Listener<A extends any[]> = (...args: A) => void;
+
+// export type Signal<A extends any[]> = {
+//   subscribe(fn: Listener<A>): () => void;
+// };
+
+// export function signal<A extends any[] = []>({
+//   listeners = new Set<any>(),
+//   emit = (fn: (...args: A) => void) => fn,
+//   subscribe = (fn: (fn: Listener<A>) => () => void) => fn,
+// } = {}) {
+//   const self: any = emit((...args: A) =>
+//     listeners.forEach((s) => s(...args))
+//   );
+
+//   self.subscribe = subscribe((fn: Listener<A>) => {
+//     listeners.add(fn);
+//     return () => {
+//       listeners.delete(fn);
+//     };
+//   });
+
+//   return self as ((...args: A) => void) & Signal<A>;
+// }
